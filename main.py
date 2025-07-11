d523c1985c7bbd2e31181526fd0dfd85eda22b8d
jiankunhan-tw <quen840404@outlook.com>
2025-07-11 21:20:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from fastapi import FastAPI, Query
-
-# ✅ 加入內建星曆設定
-from flatlib import ephembuiltin
-
 from flatlib.chart import Chart
 from flatlib.datetime import Datetime
 from flatlib.geopos import GeoPos
